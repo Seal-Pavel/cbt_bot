0add1f8ba6e94e58df55763090e8e5e6d158089d
seal <osipov.pv1000@gmail.com>
2023-07-14 22:42:23 +0300

    Init

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# from fastapi import FastAPI
+# from .api import endpoint1, endpoint2
+#
+# app = FastAPI()
+#
+# app.include_router(endpoint1.router)
+# app.include_router(endpoint2.router)
