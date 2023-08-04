70155e0feca451ec12c7d2c3fffb2c8991f7b354
seal <osipov.pv1000@gmail.com>
2023-08-04 22:55:39 +0300

    chore: release 1.0.3

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+test = 'test4'
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='cbt_bot',
+    version='1.0.3',
+    description='Когнитивно-поведенческая терапия, или КБТ, - это вид психотерапии, который помогает людям выявить и '
+                'изменить негативные мыслительные процессы и поведение.',
+    author='@pasha_os',
+    url='https://github.com/Seal-Pavel/cbt_bot',
+    packages=find_packages(),
+    install_requires=required,
+)
+# он установит в системный интерпритатор же не в виртаульное оркужение?
